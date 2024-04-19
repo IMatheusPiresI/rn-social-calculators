3d80584030689a77e469a4616c79836664563392
Matheus Pires de Sousa <matheuspdsousa@gmail.com>
2024-04-18 23:30:08 -0300

    test: create typograph unit tests component

---
+++ src/components/UI/Typograph/__tests__/Typograph.test.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react-native';
+import { WrapperTheme } from '@resources/helpers/tests/WrapperTheme';
+
+import { Typograph } from '..';
+
+describe('Button Tests', () => {
+  it('should be render correct text', () => {
+    render(
+      <WrapperTheme>
+        <Typograph>Text</Typograph>
+      </WrapperTheme>,
+    );
+
+    expect(screen.getByText('Text')).toBeTruthy();
+  });
+
+  it('should be render correct alignment', () => {
+    const alignment = 'justify';
+    render(
+      <WrapperTheme>
+        <Typograph alignment={alignment}>Text</Typograph>
+      </WrapperTheme>,
+    );
+    const typograph = screen.getByText('Text');
+    expect(typograph.props.style.textAlign).toBe(alignment);
+  });
+
+  it('should be render correct lineHeight', () => {
+    const lineHeight = 16;
+    render(
+      <WrapperTheme>
+        <Typograph lineHeight={lineHeight}>Text</Typograph>
+      </WrapperTheme>,
+    );
+    const typograph = screen.getByText('Text');
+    expect(typograph.props.style.lineHeight).toBe(lineHeight);
+  });
+});
